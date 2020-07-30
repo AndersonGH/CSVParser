6910a19a858f1e83c6146bde2e22227798bc43bf
AndersonGH <anderson_gh@bk.ru>
2020-07-30 10:29:13 +0300

    add command line support

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     try {
-        CSVFile csv("test1.csv");
+        CSVFile csv(argv[1]);
         csv.printCSV();
     } catch (const MyException & e) {
         std::cout << "Error: " <<e.what() << std::endl;
